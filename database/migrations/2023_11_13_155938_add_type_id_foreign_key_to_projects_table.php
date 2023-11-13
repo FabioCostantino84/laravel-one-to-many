717885ffa1fd1c7cbf27329c8e99cc988ac735ed
Fabio Costantino <fabiocostantino84@gmail.com>
2023-11-13 18:53:56 +0100

    add fixed seed

--- database/migrations/2023_11_13_155938_add_type_id_foreign_key_to_projects_table.php
+++ database/migrations/2023_11_13_155938_add_type_id_foreign_key_to_projects_table.php
@@ -12,7 +12,14 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('projects', function (Blueprint $table) {
-            //
+
+            $table->unsignedBigInteger('type_id')->nullable()->after('id');
+
+            $table->foreign('type_id')
+
+            ->references('id')
+
+            ->on('types');
         });
     }
 
