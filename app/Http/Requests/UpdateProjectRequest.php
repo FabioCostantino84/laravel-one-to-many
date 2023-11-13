40ee88c3b4c856c0509669ea2d0301bcc8ef0681
Fabio Costantino <fabiocostantino84@gmail.com>
2023-11-13 16:51:36 +0100

    add require title

--- app/Http/Requests/UpdateProjectRequest.php
+++ app/Http/Requests/UpdateProjectRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Symfony\Contracts\Service\Attribute\Required;
 
 class UpdateProjectRequest extends FormRequest
 {
@@ -22,7 +23,7 @@ class UpdateProjectRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'title' => 'required',
         ];
     }
 }
--- app/Models/Project.php
+++ app/Models/Project.php
@@ -4,11 +4,24 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Project extends Model
 {
     use HasFactory;
-    protected $fillable = ['title', 'slug', 'thumb', 'description', 'tech', 'github', 'link'];
+    protected $fillable =
+    [
+        'title',
+        'slug',
+        'thumb',
+        'description',
+        'tech',
+        'github',
+        'link'
+    ];
 
-    
+    public function generateSlug($title): string
+    {
+        return Str::slug($title, '-');
+    }
 }
