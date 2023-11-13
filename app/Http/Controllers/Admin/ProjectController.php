ab42f3bd578937e157160856c9f5fd44998733cd
Fabio Costantino <fabiocostantino84@gmail.com>
2023-11-13 18:00:06 +0100

    add dropdown type

--- app/Http/Controllers/Admin/ProjectController.php
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -9,6 +9,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
 use Illuminate\Http\Request;
+use App\Models\Type;
 
 
 class ProjectController extends Controller
@@ -29,7 +30,8 @@ class ProjectController extends Controller
      */
     public function create()
     {
-        return view('admin.projects.create');
+        $types = Type::all();
+        return view('admin.projects.create', compact('types'));
     }
 
     /**
--- app/Models/Project.php
+++ app/Models/Project.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Support\Str;
 
 class Project extends Model
@@ -17,11 +18,19 @@ class Project extends Model
         'description',
         'tech',
         'github',
-        'link'
+        'link',
+        'type_id'
     ];
 
     public function generateSlug($title): string
     {
         return Str::slug($title, '-');
     }
+
+    public function type(): BelongsTo
+    {
+        return $this->belongsTo(Type::class);
+    }
+
+    
 }
